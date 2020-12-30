3cae0a066f25db73e4012639f65d0ca2bfaea889
abstractmachines <abstractmachines@gmail.com>
2020-12-29 19:04:54 -0800

    Event emitter, docs.

--- events-module/event-emitter.js
+++ events-module/event-emitter.js
@@ -1,6 +1,10 @@
 /**
  * Events module
  * https://nodejs.org/api/events.html
+ *
+ * Instead of user-initiated events, Node will have events handled by events module.
+ *
+ * Add and remove listeners, on, off, etc.
  */
 
 const EventEmitter = require('events')
